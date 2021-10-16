322179a8f7b0e2705b8a3922bf9a945acee37c23
Aysel Abdullayeva <aysel2920@gmail.com>
2021-10-16 19:26:47 +0200

    Initial Commit

---
+++ CoffeeOrderingApp/CoffeeOrderingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeOrderingAppApp.swift
+//  CoffeeOrderingApp
+//
+//  Created by icelkas on 16/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeOrderingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
